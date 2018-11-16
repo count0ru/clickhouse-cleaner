6651caf8dd420a62537f22cbbe153067a18795e2
dna <dnagovitsin@life-team.net>
2018-11-16 18:09:21 +0300

    rewrite with PEP and change partition match logic

--- click_cleaner.py
+++ click_cleaner.py
@@ -2,49 +2,51 @@
 import argparse
 import datetime
 import clickhouse_driver
-# based on https://fastnetmon.com/data-retention-for-clickhouse-persistent-data-storage script
 
-parser = argparse.ArgumentParser(description='Deleting database partitions older than date')
-parser.add_argument('-d', '--database', help='database name', required=True)
-parser.add_argument('-n', '--max-days', help='remove partitions after max day count', required=True)
-parser.add_argument('-H', '--host-address', help='server address', default="127.0.0.1")
-parser.add_argument('-c', '--dry-run', help='only show all queries (not execute)', action='store_true')
-args = vars(parser.parse_args())
+PARSER = argparse.ArgumentParser(description='Deleting database partitions older than date')
+PARSER.add_argument('-d', '--database', help='database name', required=True)
+PARSER.add_argument('-n', '--max-days', help='remove partitions after max day count', required=True)
+PARSER.add_argument('-H', '--host-address', help='server address', default="127.0.0.1")
+PARSER.add_argument('-c', '--dry-run', help='only show all queries (not execute)',
+                    action='store_true')
+ARGS = vars(PARSER.parse_args())
 
-max_days = int(args['max_days'])
-dbname = args['database']
-server_addr = args['host_address']
-is_dryrun = args['dry_run']
+MAX_DAYS = int(ARGS['max_days'])
+DBNAME = ARGS['database']
+SERVER_ADDR = ARGS['host_address']
+IS_DRYRUN = ARGS['dry_run']
 
 try:
-    client = clickhouse_driver.Client(server_addr)
-except:
+    CLIENT = clickhouse_driver.Client(SERVER_ADDR)
+except TypeError:
     print "can't connect to server"
 
 try:
-    client.execute('USE ' + dbname);
-except:
+    CLIENT.execute('USE ' + DBNAME)
+except TypeError:
     print "can't execute query"
 
-now = datetime.datetime.now()
-tables = client.execute('SHOW TABLES')
- 
-for table in tables:
-    print "Process table:", table[0]
-    # Select all active partitions for this table
-    partitions = client.execute("SELECT DISTINCT partition, table FROM system.parts where active and table='" + table[0]  +"'")
-    for partition in partitions:
-        partition_name = partition[0].replace("'", "", -1)
-        partition_date = partition_name[0:4] + '-' + partition_name[5:7] +'-01 00:00:00'
-        # Parse date in format '201805'
-        partition_date = datetime.datetime.strptime(partition_date, "%Y-%m-%d %H:%M:%S")
-        delta = now - partition_date
-        if delta.days > max_days:
-            partition_delete_command = "ALTER TABLE " + dbname + "." + table[0] + " DROP PARTITION '" + partition_name + "'"
-            if is_dryrun:
-                print partition_delete_command
-            else:
-                print partition_name, " will be removed"
-                client.execute(partition_delete_command)
-                print partition_name, " now removed"
+# get current date
+NOW = datetime.datetime.now().date()
 
+# get tables list
+TABLES = CLIENT.execute('SHOW TABLES')
+
+for table in TABLES:
+    print "Process table:", table[0]
+    # select all active partitions for this table
+    get_partitions_command = ("SELECT DISTINCT partition, table, max_date FROM system.parts where active and table='%s'" % table[0])
+    print get_partitions_command
+    partitions = CLIENT.execute(get_partitions_command)
+    for partition in partitions:
+        # calculate delta between current date and max date in partition
+        delta = NOW - partition[2]
+        # do not delete partitions where partitioning not by date
+        if not partition[2] == datetime.datetime.strptime("00:00:00 1970-01-01", "%H:%M:%S %Y-%m-%d").date():
+            if delta.days > MAX_DAYS:
+                partition_delete_command = "ALTER TABLE %s.%s DROP PARTITION %s" % (DBNAME, table[0], partition[0])
+                if IS_DRYRUN:
+                    print partition_delete_command
+                else:
+                    print "%s now removed" % partition[0]
+                    CLIENT.execute(partition_delete_command)
